b40b1144903153cb42a75d3e5c720092e746d384
iaditi <aditipython@gmail.com>
2026-09-06 14:57:21 +0530

    model setup

---
+++ backend/src/services/langchain.service.ts
@@ -0,0 +1,52 @@
+import { ChatGoogle } from "@langchain/google/node";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+const model = new ChatGoogle({
+  model: "gemini-2.5-flash",
+  temperature: 0.3,
+  //lower the temperature more consistent less random answers
+});
+
+const promptTemplate = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    `
+    You are a movie recommendation expert.
+    Return high-quality recommendations based :
+    - user's request
+    - genre
+    - mood
+    - count
+    Every movie should feel intentional.
+    Do not recommend only the most obvious titles every time.
+    `,
+    //system message = who the AI + how it should behave
+    //sets the personality ans rules for the system
+  ],
+  [
+    "human",
+    `
+    User request: {userPrompt}
+    Preferences:
+    - Genre: {genre}
+    - Mood: {mood}
+    - Number of movies: {count}`,
+  ],
+]);
+
+export async function getMovieRecommendations(input: {
+     userPrompt: string;
+     genre: string;
+     mood: string;
+     count: number;
+}) {
+    const chain = promptTemplate.pipe(model)
+    const response = await chain.invoke({
+        userPrompt: input.userPrompt,
+        genre: input.genre,
+        mood: input.mood,
+        count: input.count
+    })
+    console.log("Response from LangChain:", response);
+    return response.text
+}
